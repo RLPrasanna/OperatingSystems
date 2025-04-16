47df0e8e970b8c4af2a61b1555e9d111333c24c1
Prasanna Kumar <prasanna.r1234@gmail.com>
2025-04-16 22:49:18 +0530

    Implement Adder and Subtractor for synchronization problem - Same variable is shared between 2 threads, both the threads are updating same variable in parallel can lead to race condition and give inconsistent results
    This commit introduces a synchronization problem with two classes, `Adder` and `Subtractor`, that modify a shared `Count` object. The `Adder` increments the count by adding integers from 0 to 999, while the `Subtractor` decrements it by subtracting the same range. The `Program.cs` file sets up threading to run both classes concurrently and prints the final count after execution.

---
+++ ProblemOfSynchronization/Adder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemOfSynchronization
+{
+    internal class Adder
+    {
+        private Count _count;
+        public Adder(Count count)
+        {
+            _count = count;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _count.Value += i;
+            }
+        }
+    }
+}
---
+++ ProblemOfSynchronization/Subtractor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemOfSynchronization
+{
+    internal class Subtractor
+    {
+        private Count _count;
+
+        public Subtractor(Count count)
+        {
+            _count = count;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _count.Value-=i;
+            }
+        }
+    }
+}
