6231d7e1633ab3eb75647558588fce448f0f60c0
Prasanna Kumar <prasanna.r1234@gmail.com>
2025-04-25 00:08:16 +0530

    Add exception handling and custom exception class
    Updated `Program.cs` to implement exception handling for file operations and number parsing. Introduced `WrongInputPassedException` in `WrongInputPassedException.cs` to manage specific error cases. Enhanced error messaging for various exceptions, including `IOException`, `UnauthorizedAccessException`, `FormatException`, `OverflowException`, and `DivideByZeroException`. Added a `finally` block to ensure consistent output.

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ExceptionHandling;
+
 public class Program
 {
     public static void Main(string[] args)
@@ -18,6 +20,84 @@ public class Program
         {
             Console.WriteLine(ex.Message);
         }
+        //catch (FileNotFoundException ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //}
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        //Try - Catch for parsing a number
+        try
+        {
+            Console.WriteLine("Enter a number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(b);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            Console.WriteLine("Enter x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (y == 0)
+            {
+                throw new WrongInputPassedException("Some message");
+            }
+            else
+            {
+                Console.WriteLine(x / y);
+            }
+
+            // File operations
+            // FileInfo file = new FileInfo("fileName");
+            // using (StreamReader reader = file.OpenText())
+            // {
+            //     Console.WriteLine("Line after filereader");
+            //     Console.WriteLine(reader.ReadToEnd());
+            // }
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Got DivideByZeroException");
+        }
+        catch (WrongInputPassedException)
+        {
+            Console.WriteLine("Something went wrong");
+        }
+        // catch (FileNotFoundException)
+        // {
+        //     Console.WriteLine("Got FileNotFoundException");
+        // }
+        // catch (IOException)
+        // {
+        //     Console.WriteLine("Got IOException");
+        // }
+        // catch (Exception)
+        // {
+        //     Console.WriteLine("Got General Exception");
+        // }
+        finally
+        {
+            Console.WriteLine("Inside Finally block");
+        }
+
 
     }
 
---
+++ ExceptionHandling/WrongInputPassedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class WrongInputPassedException: Exception
+    {
+        public WrongInputPassedException(string message) : base(message)
+        {
+        }
+        
+        public WrongInputPassedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
