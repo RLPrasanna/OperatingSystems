a34b7d1a0ec2edb9059c15009c9ec4bbc564f649
Prasanna Kumar <prasanna.r1234@gmail.com>
2025-04-24 23:33:23 +0530

    Add ExceptionHandling project and console application
    - Introduced a new project named "ExceptionHandling" in the solution.
    - Updated solution configuration to include build settings for the new project.
    - Created `ExceptionHandling.csproj` targeting .NET 8.0 with implicit usings and nullable reference types enabled.
    - Added `Program.cs` with a console app that reads user input and handles file reading with exception management.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Reading user input
+        Console.WriteLine("Enter a number:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(a);
+
+        //Try - Catch for reading a file
+        try
+        {
+            ReadFile("abc.txt");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+    private static void ReadFile(string path)
+    {
+        FileInfo file = new FileInfo(path);
+        using (StreamReader reader = file.OpenText())
+        {
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+}
